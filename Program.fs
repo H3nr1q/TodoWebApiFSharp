9f343ca3d88e131838729f1baf4332d8e4adf79b
Carlos Henrique <carlos.henrique@luz-ef.com>
2022-02-21 17:26:49 -0300

    first commit

---
+++ Program.fs
@@ -0,0 +1,37 @@
+﻿module Todos.Program
+
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open Todos.Http
+open System.Collections
+
+
+let routes =
+  choose [
+    TodoHttp.handlers ]
+
+let configureApp (app : IApplicationBuilder) =
+  app.UseGiraffe routes
+
+let configureServices (services : IServiceCollection) =
+  let inMemory = Hashtable()
+  services.AddSingleton<TodoFind>(TodoInMemory.find inMemory) |> ignore
+  services.AddSingleton<TodoSave>(TodoInMemory.save inMemory) |> ignore  
+
+[<EntryPoint>]
+let main _ =
+  WebHostBuilder()
+    .UseKestrel()
+    .Configure(Action<IApplicationBuilder> configureApp)
+    .ConfigureServices(configureServices)
+    .Build()
+    .Run()
+  0
+
+  
+
+
+
